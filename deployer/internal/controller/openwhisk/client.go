c8ec89929f6abd3f7338b06c520d806af26699e9
Giannis Fakinos <45902117+john98nf@users.noreply.github.com>
2021-07-21 19:14:05 +0300

    WIP: greedy controller - error from watcher client

---
+++ deployer/internal/controller/openwhisk/client.go
@@ -0,0 +1,60 @@
+// Copyright © 2021 Giannis Fakinos
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type watcherClientInterface interface {
+	FunctionInvokationNotice(function string) error
+	// FuctionSpeedUpRequest(function string) error
+	// FuctionSlowDownRequest(function string) error
+}
+
+type WatcherClient struct {
+	endpoint string `json:endpoint,omitempty`
+}
+
+func NewWatcherClient() *WatcherClient {
+	return &WatcherClient{
+		endpoint: "scw-watcher-sprm-svc:8080/api",
+	}
+}
+
+func (client *WatcherClient) FunctionInvokationNotice(function string) error {
+	resp, err := http.Get(client.endpoint + "/function/" + function)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode == 200 {
+		return nil
+	} else {
+		return fmt.Errorf(string(body))
+	}
+}
--- deployer/internal/controller/openwhisk/main.go
+++ deployer/internal/controller/openwhisk/main.go
@@ -30,17 +30,20 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/apache/openwhisk-client-go/whisk"
 )
 
 type controller (func(map[string]interface{}) map[string]interface{})
 
+const TARGET_LATENCY time.Duration = 50000
+
 var (
 	client         *whisk.Client
 	controllerType = map[string]controller{
-		//"greedy": greedyControl,
-		"dummy": dummyControl,
+		"greedy": greedyControl,
+		"dummy":  dummyControl,
 	}
 )
 
@@ -55,6 +58,11 @@ func Main(obj map[string]interface{}) map[string]interface{} {
 	return controllerType[ALGORITHM_TYPE](obj)
 }
 
+/*
+	No actual control over function invocation,
+	as it just invokes each function.
+	Used for benchmarking purposes and referrence point.
+*/
 func dummyControl(obj map[string]interface{}) map[string]interface{} {
 	aRes := obj
 	for i, f := range functionList {
@@ -62,4 +70,63 @@ func dummyControl(obj map[string]interface{}) map[string]interface{} {
 		aRes, _, _ = client.Actions.Invoke(f, aRes, true, true)
 	}
 	return aRes
+}
+
+/*
+	Greedy control.
+	Each function invokation execution time is been monitored and its actual elapsed time is been compared
+	with a referrence point (pre-configured). Their difference is called slack.
+	A positive slack means that the next function invokation may run with fewer resources (slow down).
+	A negative slack means that the next fuction must run with more resources (speed up).
+*/
+func greedyControl(obj map[string]interface{}) map[string]interface{} {
+	var (
+		tStart  time.Time
+		tEnd    time.Time
+		elapsed time.Duration
+		slack   time.Duration = 0
+	)
+
+	profiledExecutionTime := distributeLatency(TARGET_LATENCY, len(functionList))
+
+	watcherClient := NewWatcherClient()
+	aRes := obj
+	n := len(functionList)
+	for i, f := range functionList {
+		log.Printf("Invoking function-%v %v\n", i, f)
+		tStart = time.Now()
+		aRes, _, _ = client.Actions.Invoke(f, aRes, true, true)
+		tEnd = time.Now()
+		elapsed = tEnd.Sub(tStart)
+		slack += profiledExecutionTime[i] - elapsed
+
+		if i < n-1 {
+			if slack > 0 {
+				// TO DO: Actual slow down call to watcher supreme
+				log.Printf("Positive slack %v\n", slack)
+				if err := watcherClient.FunctionInvokationNotice(functionList[i+1]); err != nil {
+					log.Printf("Error from watcher client: %v\n", err.Error())
+				}
+			} else if slack < 0 {
+				// TO DO: Actual speed up call to watcher supreme
+				log.Printf("Negative slack %v\n", slack)
+				if err := watcherClient.FunctionInvokationNotice(functionList[i+1]); err != nil {
+					log.Printf("Error from watcher client: %v\n", err.Error())
+				}
+			}
+		}
+	}
+	return aRes
+}
+
+func distributeLatency(latency time.Duration, n int) []time.Duration {
+	res := make([]time.Duration, n)
+	t := latency / time.Duration(n)
+	for i := range res {
+		res[i] = t
+	}
+	if mod := latency % time.Duration(n); mod != 0 {
+		res[n-1] = mod
+	}
+	return res
 }
