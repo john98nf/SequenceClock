cdce2ff2a43dee0b144ed06ca1ee72e8008473b5
Giannis Fakinos <45902117+john98nf@users.noreply.github.com>
2021-07-22 14:36:36 +0300

    WIP: get function container info from watcher

--- watcher/main.go
+++ watcher/main.go
@@ -21,11 +21,20 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"os"
+	"regexp"
 
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
+)
+
+const (
+	REG_EXP1 string = `(^\/k8s_user-action_wskowdev-invoker-00-[0-9][0-9]-guest-%v_openwhisk_)`
+	REG_EXP2 string = `(^\/k8s_POD_wskowdev-invoker-00-[0-9][0-9]-guest-%v_openwhisk_)`
 )
 
 var (
@@ -40,9 +49,11 @@ func main() {
 	{
 		// GET Request http://localhost:8080/api/check
 		apiWatcher.GET("/check", check)
+		// GET Request http://localhost:8080/api/function/:name
+		apiWatcher.GET("/function/:name", getContainer)
 		// PATCH Request http://localhost:8080/api/function/:name/speedUp
 		apiWatcher.PATCH("/function/:name/speedUp", speedUp)
-		// PATCH Request http://localhost:8080/api/function/:name/slowDown?name=x
+		// PATCH Request http://localhost:8080/api/function/:name/slowDown
 		apiWatcher.PATCH("/function/:name/slowDown", slowDown)
 	}
 
@@ -80,18 +91,51 @@ func slowDown(c *gin.Context) {
 	c.String(http.StatusOK, "Watcher %v: SpeedUp request for function %v.", hostIP, functionName)
 }
 
-// func test(c *gin.Context) {
+/*
+	Get information for function related
+	container.
+*/
+func getContainer(c *gin.Context) {
+	f := c.Param("name")
+	podType := c.DefaultQuery("type", "running")
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
 
-// 	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
-// 	if err != nil {
-// 		c.String(http.StatusInternalServerError, err.Error())
-// 		return
-// 	}
+	data := make(gin.H)
+	var exp string
+	if podType == "running" {
+		exp = fmt.Sprintf(REG_EXP1, f)
+	} else if podType == "paused" {
+		exp = fmt.Sprintf(REG_EXP2, f)
+	} else {
+		c.String(http.StatusBadRequest, "Not supported pod type.")
+		return
+	}
 
-// 	data := make(gin.H)
-// 	for _, c := range containers {
-// 		data[c.ID[:10]] = c.Names
-// 	}
-// 	c.JSON(200, data)
+	for _, cnt := range containers {
+		if matched, err := regexp.MatchString(exp, cnt.Names[0]); matched {
+			data["ID"] = cnt.ID[:10]
+			data["Names"] = cnt.Names
+			data["Image"] = cnt.Image
+			data["Command"] = cnt.Command
+			data["Created"] = cnt.Created
+			data["Ports"] = cnt.Ports
+			data["SizeRw"] = cnt.SizeRw
+			data["SizeRootFs"] = cnt.SizeRootFs
+			data["Labels"] = cnt.Labels
+			data["State"] = cnt.State
+			data["Status"] = cnt.Status
+			data["NetworkMode"] = cnt.HostConfig.NetworkMode
+			// Ignored NetworkSettings
+			data["Mount"] = cnt.Mounts
+		} else if err != nil {
+			c.String(http.StatusInternalServerError, err.Error())
+			return
+		}
+	}
+	c.JSON(200, data)
 
-// }
+}
