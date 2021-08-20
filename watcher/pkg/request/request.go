92d4ef6a3a05a96eea1562011f9f5f3a2d97b1ec
Giannis Fakinos <45902117+john98nf@users.noreply.github.com>
2021-08-20 23:51:38 +0300

    gorilla schema tags added to request struct

--- watcher/pkg/request/request.go
+++ watcher/pkg/request/request.go
@@ -1,20 +1,20 @@
 package request
 
 type Request struct {
-	Function string   `form:"function" binding:"required"`
-	Metrics  *Metrics `form:"metrics" binding:"required"`
+	Function string   `form:"function" binding:"required" schema:"function"`
+	Metrics  *Metrics `form:"metrics" binding:"required" schema:"metrics"`
 }
 
 type ResetRequest struct {
-	Id       uint64 `form:"id" binding:"required"`
-	Function string `form:"function" binding:"required"`
+	Id       uint64 `form:"id" binding:"required" schema:"id"`
+	Function string `form:"function" binding:"required" schema:"function"`
 }
 
 type Metrics struct {
-	Slack                 int64 `form:"slack" binding:"required"`         // Used by P controller
-	SumOfSlack            int64 `form:"sumOfSlack" binding:"required"`    // Used by I controller
-	PreviousSlack         int64 `form:"previousSlack" binding:"required"` // Used by D controller
-	ProfiledExecutionTime int64 `form:"profiledExecutionTime" binding:"required"`
+	Slack                 int64 `form:"slack" binding:"required" schema:"slack"`                 // Used by P controller
+	SumOfSlack            int64 `form:"sumOfSlack" binding:"required" schema:"sumfSlack"`        // Used by I controller
+	PreviousSlack         int64 `form:"previousSlack" binding:"required" schema:"previousSlack"` // Used by D controller
+	ProfiledExecutionTime int64 `form:"profiledExecutionTime" binding:"required" schema:"profiledExecutionTime"`
 }
 
 func NewResetRequest(id uint64, function string) *ResetRequest {
