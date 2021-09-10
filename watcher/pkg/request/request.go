f8ef384f597cd968a248a298b31202c8a005c974
Giannis Fakinos <45902117+john98nf@users.noreply.github.com>
2021-09-10 11:26:06 +0300

    don't make changes late at night, or else everything breaks

--- watcher/pkg/request/request.go
+++ watcher/pkg/request/request.go
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-package main
+package github.com/john98nf/SequenceClock/watcher/pkg/request
 
 /*
 	Initial Request struct made by sequence controller
